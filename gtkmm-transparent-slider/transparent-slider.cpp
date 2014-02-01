e0af0d62ff1303fddfc98bbd54c02d1c916948aa
AthanasiusOfAlex <lmelahn@legionaries.org>
2014-02-01 18:17:17 +0100

    Update transparent-slider.cpp

--- gtkmm-transparent-slider/transparent-slider.cpp
+++ gtkmm-transparent-slider/transparent-slider.cpp
@@ -1,7 +1,21 @@
 /**
  * transparent-slider.cpp
  *
- * Louis Melahn, L.C. January 31, 2014.
+ * This file is part of Transparent-Slider.
+ * Transparent-Slider is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  Transparent-Slider is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Transparent-Slider.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  (c) Louis Melahn, L.C. February 1, 2014.
  */
 #include "transparent-slider.h"
 
